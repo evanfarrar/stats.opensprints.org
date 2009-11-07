e2c6897873a8019d2f75c7487d5c0712c9888b44
Evan Farrar <evanfarrar@gmail.com>
2009-11-07 11:26:22 -0600

    Email password file.

--- config/initializers/action_mailer_configs.rb
+++ config/initializers/action_mailer_configs.rb
@@ -1,9 +1,11 @@
+MAILSERVER_FILE = File.join(RAILS_ROOT, '..', '..', 'shared', 'config', 'mailserverpassword') %>
+
 ActionMailer::Base.smtp_settings = {
   :address => 'mail.opensprints.com',
   :port => 587,
   :domain => 'opensprints.com',
   :authentication => :login,
   :user_name => 'system@opensprints.com',
-  :password => 'password'
+  :password => (File.read(MAILSERVER_FILE).chomp if File.readable?(MAILSERVER_FILE))
 }
 
